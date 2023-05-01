b228a3793037d3291aebc8ffa643d64ffe611047
Samsung <bitup.games@gmail.com>
2023-05-01 20:15:47 +0200

    add: Modal and CloseButton components

---
+++ src/components/Layout/CloseButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="absolute top-3 right-5" @click="onClose">
+    <v-icon class="h-4 w-4" name="hi-x" />
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  onClose: {
+    type: Function,
+    required: true
+  }
+})
+
+</script>
---
+++ src/components/Layout/Modal.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="fixed inset-0 z-10 flex">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+</script>
