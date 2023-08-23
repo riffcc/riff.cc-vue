8393b57dfdda5454dafa560fbb4648a709dc6a28
en0c-026 <bitup.games@gmail.com>
2023-08-23 13:24:55 +0200

    update: tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,14 @@ module.exports = {
       colors: {
         "video-current-progress": "#5558DD",
         "video-progress": "#343434",
-      }
+        "background": "#221F1F",
+        "background-secondary": "#141414",
+        "background-secondary-alt": "#191919",
+        "primary": "#A020F0"
+      },
+      fontFamily: {
+        'app': ['Josefin Sans', 'sans-serif'], // Reemplaza 'Open Sans' con el nombre de la fuente que importaste
+      },
     },
   },
   plugins: [],
