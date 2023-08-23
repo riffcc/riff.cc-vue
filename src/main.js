bed8002563cda2d4444663da81b310addc8fe43a
en0c-026 <bitup.games@gmail.com>
2023-08-23 13:19:02 +0200

    add: new icons

--- src/main.js
+++ src/main.js
@@ -33,7 +33,24 @@ import {
   HiFastForward,
   HiRewind,
   MdFullscreen,
-  HiArrowCircleLeft
+  HiArrowCircleLeft,
+  FaTwitter,
+  FaInstagram,
+  FaSkype,
+  FaFacebook,
+  MdNotificationsnoneOutlined,
+  MdSearch,
+  MdKeyboardarrowup,
+  MdPlayarrowRound,
+  MdLibraryadd,
+  MdMorevert,
+  MdPlaycirclefilled,
+  RiSkipBackFill,
+  RiSkipForwardFill,
+  MdPausecirclefilled,
+  PrReplay,
+  IoVolumeMedium,
+  IoVolumeOff
  } from "oh-vue-icons/icons";
 
 const cache = new InMemoryCache({
@@ -139,6 +156,8 @@ const app = createApp({
   },
   render: () => h(App)
 })
+
+export const IoShuffle = { "name": "io-shuffle", "minX": 0, "minY": 0, "width": 512, "height": 512, "raw": "<path fill=\"none\" stroke=\"#fff\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"32\" d=\"M400 304l48 48-48 48M400 112l48 48-48 48M64 352h85.19a80 80 0 0066.56-35.62L256 256\"/><path fill=\"none\" stroke=\"#fff\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"32\" d=\"M64 160h85.19a80 80 0 0166.56 35.62l80.5 120.76A80 80 0 00362.81 352H416M416 160h-53.19a80 80 0 00-66.56 35.62L288 208\"/>" };
 addIcons(
   HiCheckCircle,
   HiXCircle,
@@ -163,7 +182,25 @@ addIcons(
   HiRewind,
   HiFastForward,
   MdFullscreen,
-  HiArrowCircleLeft
+  HiArrowCircleLeft,
+  FaTwitter,
+  FaInstagram,
+  FaSkype,
+  FaFacebook,
+  MdSearch,
+  MdNotificationsnoneOutlined,
+  MdKeyboardarrowup,
+  MdPlayarrowRound,
+  MdLibraryadd,
+  MdMorevert,
+  MdPlaycirclefilled,
+  RiSkipBackFill,
+  RiSkipForwardFill,
+  MdPausecirclefilled,
+  IoShuffle,
+  PrReplay,
+  IoVolumeMedium,
+  IoVolumeOff
 );
 
 app.component("v-icon", OhVueIcon);
