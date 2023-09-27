77b4c115274de5f0ec0e5bf4f03e742769e8df6e
en0c-026 <bitup.games@gmail.com>
2023-09-27 16:24:47 +0200

    update: refactor pieceItem

--- src/components/PieceItem.vue
+++ src/components/PieceItem.vue
@@ -1,27 +1,8 @@
 <template>
-  <div>
-    <div class="h-28 w-28 md:h-32 md:w-32 relative mx-auto border rounded-xl">
-      <a
-        v-if="pin.piece.details?.thumbnailCid"
-        :href="`https://ipfs.io/ipfs/${pin.piece.details?.thumbnailCid}`" 
-        target="_blank"
-      >
-        <img class="absolute w-full h-full rounded-xl" :alt="''" :src="`https://${ipfsGateway}/ipfs/${pin.piece.details?.thumbnailCid}`" />
-      </a>
-      <a
-          v-else-if="pin.piece?.contentCid"
-          :href="`https://ipfs.io/ipfs/${pin.piece?.contentCid}`" 
-          target="_blank"
-        >
-          <img class="absolute w-full h-full rounded-xl" :alt="''" :src="`https://${ipfsGateway}/ipfs/${pin.piece?.contentCid}`" />
-        </a>
-      <div v-else class="flex h-full">
-        <v-icon name="pr-image" class="h-20 w-20 m-auto text-slate-300" />
-      </div>
-    </div>
-    <button @click="handleOnClick" class="w-full h-8">
-      <p class="text-xs md:text-sm mt-2 text-center font-medium text-slate-50 hover:cursor-pointer">{{ pin.piece?.name }}</p>
-    </button>
+  <div class="border h-28 w-28 md:h-32 md:w-32">
+    <img v-if="pin.piece.details?.thumbnailCid" class="w-full h-full" :alt="''" :src="`https://${ipfsGateway}/ipfs/${pin.piece.details?.thumbnailCid}`" />
+    <img v-else-if="pin.piece?.contentCid" class="w-full h-full" :alt="''" :src="`https://${ipfsGateway}/ipfs/${pin.piece?.contentCid}`" />
+    <p @click="handleOnClick" class="text-xs md:text-sm mt-2 text-center font-medium text-slate-50 hover:cursor-pointer">{{ pin.piece?.name }}</p>
   </div>
 </template>
 
