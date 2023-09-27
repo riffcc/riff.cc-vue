d7e616527a00ad57b6f76a371495968a9fde347d
en0c-026 <bitup.games@gmail.com>
2023-09-27 11:08:18 +0200

    update: use axios

--- src/utils/callAdminServer.js
+++ src/utils/callAdminServer.js
@@ -1,12 +1,16 @@
-export default async function callAdminServer(url, params) {
-  const response = await fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(params),
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-  });
-  return await response.json();
+import axios from 'axios';
 
-};
+export default async function callAdminServer(url, params) {
+  try {
+    const response = await axios.post(url, params, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error on callAdminServer:', error);
+  }
+}
+
