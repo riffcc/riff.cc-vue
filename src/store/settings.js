b5eeed13e5c7dd359565aa035d9099d56d388bf4
en0c-026 <bitup.games@gmail.com>
2023-10-25 19:04:07 +0200

    add: dinamic title and navicon

--- src/store/settings.js
+++ src/store/settings.js
@@ -1,6 +1,7 @@
+import { useFavicon, useTitle } from '@vueuse/core'
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
-
+import { computed, ref } from 'vue'
+import { IPFS_GATEWAY } from '@/config/constants'
 export const useSettingsStore = defineStore('settingsStore', () => {
   const show = ref(false)
   const autoplay = ref(false)
@@ -11,7 +12,10 @@ export const useSettingsStore = defineStore('settingsStore', () => {
 
   const open = () => show.value = true
   const hide = () => show.value = false
-
+  const siteTile = computed(() => siteName ? siteName.value : 'My Site')
+  const siteNavicon = computed(() => siteImage ? `https://${IPFS_GATEWAY}/ipfs/${siteImage.value}` : null)
+  useTitle(siteTile)
+  useFavicon(siteNavicon)
   return {
     show,
     open,
